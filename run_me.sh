23cdfbb87b411e48b02c03efff3679050523465b
Henry Post <HenryFBP@gmail.com>
2018-10-15 21:04:04 -0500

    add demo for mac/linux

---
+++ run_me.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pipenv install
+
+pipenv run python classify_image.py --image_file=images/dumbbell.jpg
+pipenv run python classify_image.py --image_file=images/treadmill.jpg
+pipenv run python classify_image.py --image_file=images/football.jpg
+pipenv run python classify_image.py --image_file=images/soccer_ball.jpg
