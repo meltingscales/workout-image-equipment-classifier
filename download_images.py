39c8ec29fcc6d4748eb5b5573fb76d80df1520e4
HenryFBP <HenryFBP@gmail.com>
2018-10-16 18:57:57 -0500

    add back in validation of picture files (+2 squashed commit)
    Squashed commit:
    
    [cb29363] 2 slow
    
    [8bd3e0c] sort keywords

--- download_images.py
+++ download_images.py
@@ -1,3 +1,4 @@
+import sys
 import os
 
 from PIL import Image
@@ -7,9 +8,30 @@ BASE_DIR = os.path.join(os.path.dirname(__file__), 'data')
 DOWNLOAD_DIR = os.path.join(BASE_DIR, 'downloaded_images')
 
 IMAGE_LIMIT = 100  # How many images should we download?
-ERROR_TOLERANCE = (IMAGE_LIMIT // 5)  # Threshold for ignoring the fact we have missing/crappy/broken images
+ERROR_TOLERANCE = (IMAGE_LIMIT // 5)  # What to do if we miss a few?
 
-DELAY = 1  # How long do we wait in seconds between downloading images?
+DELAY = 1  # How long do we wait between downloading images?
+
+
+def sort_file(path: str):
+    sorted_stuff = []
+
+    # read lines
+    with open(path, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            sorted_stuff.append(line)
+
+    sorted_stuff = sorted(sorted_stuff)
+
+    with open(path, 'w') as f:
+        for i in range(0, len(sorted_stuff)):
+            item = sorted_stuff[i]
+
+            f.write(item)
+
+            if i + 1 < len(sorted_stuff):
+                f.write('\n')
 
 
 def number_of_files(path: str) -> int:
@@ -21,20 +43,19 @@ def valid_picture_file(path: str, valid_formats=frozenset(['JPEG', 'JPG', 'PNG',
         img = Image.open(path)
         img.verify()
         itype = img.format
-
         # If it's not in the list of formats we allow for
         if itype.upper() not in [format.upper() for format in valid_formats]:
             return False
-
     except(IOError, SyntaxError) as e:
         return False
-
     return True
 
 
 if __name__ == '__main__':
     if not os.path.exists(DOWNLOAD_DIR):
         os.mkdir(DOWNLOAD_DIR)
+
+    sort_file('data/keywords.txt')
 
     with open('data/keywords.txt', 'r') as file:
         for item in file.readlines():
@@ -48,18 +69,17 @@ if __name__ == '__main__':
                 'print_paths': False,
                 'output_directory': DOWNLOAD_DIR,
                 'delay': DELAY,
-                'format': 'jpg',
             }
 
+            filenum = number_of_files(os.path.join(DOWNLOAD_DIR, item))
             itemdir = os.path.join(DOWNLOAD_DIR, item)
-            filenum = number_of_files(itemdir)
 
             if filenum + ERROR_TOLERANCE < IMAGE_LIMIT:
-                print(f" DL ( {item:20s} )[{filenum:^5d}<{IMAGE_LIMIT:^5d}] ± {ERROR_TOLERANCE}")
+                print(f' DL ( {item:20s} )[{filenum:^5d}<{IMAGE_LIMIT:^5d}] ± {ERROR_TOLERANCE}')
                 paths = response.download(args)
-
+                print(paths)
             else:
-                print(f"SKIP( {item:20s} )[{filenum:^5d}/{IMAGE_LIMIT:^5d}] ± {ERROR_TOLERANCE}")
+                print(f'SKIP( {item:20s} )[{filenum:^5d}/{IMAGE_LIMIT:^5d}] ± {ERROR_TOLERANCE}')
 
             # Verify they're not corrupted/garbage image files
             for filename in os.listdir(itemdir):
