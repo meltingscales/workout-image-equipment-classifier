dc85d0093ef9395146edc134699032a68880c3e1
Henry Post <HenryFBP@gmail.com>
2018-10-17 17:17:16 -0500

    move all images to /data folder

--- demo_image_recognition_default_neural_net.bat
+++ demo_image_recognition_default_neural_net.bat
@@ -1,6 +1,6 @@
-pipenv run python classify_image.py --image_file=images/dumbbell.jpg
-pipenv run python classify_image.py --image_file=images/treadmill.jpg
-pipenv run python classify_image.py --image_file=images/football.jpg
-pipenv run python classify_image.py --image_file=images/soccer_ball.jpg
+pipenv run python classify_image.py --image_file=data/real_images/dumbbell.jpg
+pipenv run python classify_image.py --image_file=data/real_images/treadmill.jpg
+pipenv run python classify_image.py --image_file=data/real_images/football.jpg
+pipenv run python classify_image.py --image_file=data/real_images/soccer_ball.jpg
 
 PAUSE
--- demo_image_recognition_retrained_neural_net.bat
+++ demo_image_recognition_retrained_neural_net.bat
@@ -3,6 +3,6 @@ pipenv run python label_image.py^
  --labels=./retrained_model/output_labels.txt^
  --input_layer=Placeholder^
  --output_layer=final_result^
- --image=./images/treadmill.jpg
+ --image=./data/real_images/treadmill.jpg
 
 PAUSE
