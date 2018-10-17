08ba32f959963a2e0db16df8aa0a0933084ba432
Henry Post <HenryFBP@gmail.com>
2018-10-17 12:33:20 -0500

    remove PAUSE from retrain.sh
    Can you tell I don't use Linux for dev?

--- retrain.sh
+++ retrain.sh
@@ -4,5 +4,3 @@ pipenv run python retrain.py --image_dir data/downloaded_images/ --logdir /tmp/r
 
 cp /tmp/output_graph.pb   ./retrained_model/output_graph.pb
 cp /tmp/output_labels.txt ./retrained_model/output_labels.txt
-
-PAUSE
