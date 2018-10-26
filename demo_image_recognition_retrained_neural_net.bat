f354f0109a0058dfa42eab848768e55c9840d239
Henry Post <HenryFBP@gmail.com>
2018-10-25 21:10:22 -0500

    update demo image recognition to use all test images

--- demo_image_recognition_retrained_neural_net.bat
+++ demo_image_recognition_retrained_neural_net.bat
@@ -1,8 +1,12 @@
-pipenv run python label_image.py^
- --graph=./retrained_model/output_graph.pb^
- --labels=./retrained_model/output_labels.txt^
- --input_layer=Placeholder^
- --output_layer=final_result^
- --image=./data/real_images/treadmill.jpg
+
+FOR %%X IN ( data/real_images/* ) DO (
+
+	pipenv run python label_image.py^
+	--graph=./retrained_model/output_graph.pb^
+	--labels=./retrained_model/output_labels.txt^
+	--input_layer=Placeholder^
+	--output_layer=final_result^
+	--image=./data/real_images/%%X
+)
 
 PAUSE
--- demo_image_recognition_retrained_neural_net.sh
+++ demo_image_recognition_retrained_neural_net.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+for filename in ./data/real_images/*; do
+	pipenv run python label_image.py\
+	 --graph=./retrained_model/output_graph.pb\
+	 --labels=./retrained_model/output_labels.txt\
+	 --input_layer=Placeholder\
+	 --output_layer=final_result\
+	 --image=./data/real_images/$filename
+done
 
-pipenv run python label_image.py\
- --graph=./retrained_model/output_graph.pb\
- --labels=./retrained_model/output_labels.txt\
- --input_layer=Placeholder\
- --output_layer=final_result\
- --image=./data/real_images/treadmill.jpg
-
-PAUSE
