cec78a8e532243693383f703eebd559e95b35dde
Henry Post <HenryFBP@gmail.com>
2018-11-09 17:19:37 -0600

    fix app's dark theme

--- android_apps/tensorflow_for_poets_tflite/app/src/main/java/com/scanfit/activities/HomeActivity.java
+++ android_apps/tensorflow_for_poets_tflite/app/src/main/java/com/scanfit/activities/HomeActivity.java
@@ -109,7 +109,6 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        setTheme(R.style.Theme_AppCompat);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
