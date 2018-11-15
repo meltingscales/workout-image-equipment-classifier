fc097bcd720708112c22940865e318d030c65fdf
Henry Post <HenryFBP@gmail.com>
2018-11-15 10:56:13 -0600

    default neural net demo now uses all real_images

--- demo_image_recognition_default_neural_net.bat
+++ demo_image_recognition_default_neural_net.bat
@@ -1,6 +1,7 @@
-pipenv run python classify_image.py --image_file=data/real_images/dumbbell.jpg
-pipenv run python classify_image.py --image_file=data/real_images/treadmill.jpg
-pipenv run python classify_image.py --image_file=data/real_images/football.jpg
-pipenv run python classify_image.py --image_file=data/real_images/soccer_ball.jpg
+FOR %%X IN ( data/real_images/* ) DO (
 
+	pipenv run python classify_image.py^
+	--image_file=data/real_images/%%X
+
+)
 PAUSE
